32e54e4285c117fdd1f4e408deda8408f59c0023
mandatorySuicide <najim.ju@gmail.com>
2015-01-21 19:08:24 +0600

    SELISEOJexp execute dll file successfully

---
+++ src/SELISEOJexp/SELISEOJexp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SELISEOJexp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //string com = "/c " + @"D:\SeliseOJexp\b.bat";
+                //string com2 = "/c " + @"D:\SeliseOJexp\in.txt";
+                //System.Diagnostics.Process.Start("cmd.exe",com2);
+
+                Process p = new Process();
+
+                p.StartInfo.FileName = "cmd.exe";
+
+                p.StartInfo.Arguments = @"/C D:\SeliseOJexp\b.bat";
+                p.StartInfo.WorkingDirectory = @"D:\SeliseOJexp\";
+                p.StartInfo.Verb = "runas";
+
+                p.Start();
+
+                //p.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                string msg = e.Message;
+            }
+        }
+    }
+}
