d9f87d9d4032b5a3aa1f905e903ed8b0ba9ef173
Najim Ahmed <najim.ju@gmail.com>
2015-03-14 02:23:00 +0600

    after tutorial (filter)

--- src/EggHead ngPractice/EggHead ngPractice/EggHead ngPractice.WebClient/CustomFilter/CustomFilter.Filter.js
+++ src/EggHead ngPractice/EggHead ngPractice/EggHead ngPractice.WebClient/CustomFilter/CustomFilter.Filter.js
@@ -1,6 +1,8 @@
 ﻿angular.module('custom.filter').filter('incrementFilter',function() {
+   
+
     return function (input, index, incrementvalue) {
-        console.log(input,index,incrementvalue);
-        return input[index-1] + incrementvalue;
+        console.log(input, index, incrementvalue);
+        return input[index - 1] + incrementvalue;
     }
 });
