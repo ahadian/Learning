6844cf57ff690e0fcb8b6bde9e3dd596548739db
Najim Ahmed <najim.ju@gmail.com>
2015-03-22 02:27:30 +0600

    update

--- src/EggHead ngPractice/EggHead ngPractice/EggHead ngPractice.WebClient/CustomFilter/CustomFilter.App.js
+++ src/EggHead ngPractice/EggHead ngPractice/EggHead ngPractice.WebClient/CustomFilter/CustomFilter.App.js
@@ -1,1 +1,1 @@
-﻿angular.module('custom.filter', ['ngRoute', 'ngResource']);
+﻿angular.module('custom.filter', []);
--- src/EggHead ngPractice/EggHead ngPractice/EggHead ngPractice.WebClient/CustomFilter/CustomFilter.Filter.js
+++ src/EggHead ngPractice/EggHead ngPractice/EggHead ngPractice.WebClient/CustomFilter/CustomFilter.Filter.js
@@ -1,8 +1,13 @@
 ﻿angular.module('custom.filter').filter('incrementFilter',function() {
-   
-
     return function (input, index, incrementvalue) {
         console.log(input, index, incrementvalue);
         return input[index - 1] + incrementvalue;
+    }
+});
+
+angular.module('custom.filter').filter('GuruGiveMeLength', function () {
+    return function (input) {
+        console.log(input);
+        return input.length;
     }
 });
