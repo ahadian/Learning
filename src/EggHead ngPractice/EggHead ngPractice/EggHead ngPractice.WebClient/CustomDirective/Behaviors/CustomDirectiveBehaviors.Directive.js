0c488d9444293134d57f0393393a2589566a8fdc
Najim Ahmed <najim.ju@gmail.com>
2015-04-26 21:18:38 +0600

    behavior done

--- src/EggHead ngPractice/EggHead ngPractice/EggHead ngPractice.WebClient/CustomDirective/Behaviors/CustomDirectiveBehaviors.Directive.js
+++ src/EggHead ngPractice/EggHead ngPractice/EggHead ngPractice.WebClient/CustomDirective/Behaviors/CustomDirectiveBehaviors.Directive.js
@@ -1,78 +1,80 @@
-﻿angular.module('custom.directive').directive('mouseEnter',function() {
-    // best practice is always return object from directive function instead of function
-    return {
-        restrict: 'E',
-        link: function(scope,element) {
-            //console.log(scope, 'scope', element, 'element');
-            //element.bind("mouseenter",function() {
-            //    console.log('Mouse Entered!');
-            //});
-            console.log(element.length);
-            element[0].onmouseover= function () {
-                console.log('Mouse Entered!');
-            };
-        }
+﻿angular.module('custom.directive').controller('myCtrl', ['$scope',function($scope) {
+    $scope.sayHi = function() {
+        console.log('Hi Cortana');
     }
-});
+    $scope.sayBye = function () {
+        console.log('Bye Cortana');
+    }
+}]);
 
 angular.module('custom.directive').directive('mouseEnter', function () {
     // best practice is always return object from directive function instead of function
     return {
         restrict: 'E',
-        link: function (scope, element) {
-            //console.log(scope, 'scope', element, 'element');
-            //element.bind("mouseenter",function() {
+        link: function (scope, element,attrs) {
+            console.log(attrs);
+            //element[0].onmouseover = function () {
             //    console.log('Mouse Entered!');
-            //});
-            console.log(element.length);
-            element[0].onmouseover = function () {
-                console.log('Mouse Entered!');
-            };
+            //};
+            
+            element.bind("mouseenter", function () {
+                element.removeClass("redClass");
+                element.addClass("blueClass");
+                //console.log('Mouse Entered!', attrs);
+                scope.$apply(attrs.invokeHi);
+
+            });
+            element.on("mouseleave", function () {
+                element.addClass("redClass");
+                element.removeClass("blueClass");
+                //console.log('Mouse Leaved!');
+                scope.$apply(attrs.invokeBye);
+            });
         }
     }
 });
 
 /*
-onabort: null
-onautocomplete: null
-onautocompleteerror: null
-onbeforecopy: null
-onbeforecut: null
-onbeforepaste: null
-onblur: null
-oncancel: null
-oncanplay: null
-oncanplaythrough: null
-onchange: null
-onclick: null
-onclose: null
-oncontextmenu: null
-oncopy: null
-oncuechange: null
-oncut: null
-ondblclick: null
-ondrag: null
-ondragend: null
-ondragenter: null
-ondragleave: null
-ondragover: null
-ondragstart: null
-ondrop: null
-ondurationchange: null
-onemptied: null
-onended: null
-onerror: null
-onfocus: null
-oninput: null
-oninvalid: null
-onkeydown: null
-onkeypress: null
-onkeyup: null
-onload: null
-onloadeddata: null
-onloadedmetadata: null
-onloadstart: null
-onmousedown: null
+onabort: null ~
+onautocomplete: null~
+onautocompleteerror: null!~
+onbeforecopy: null~
+onbeforecut: null~
+onbeforepaste: null~
+onblur: null~
+oncancel: null~
+oncanplay: null~
+oncanplaythrough: null~
+onchange: null~
+onclick: null~!
+onclose: null~
+oncontextmenu: null~
+oncopy: null~
+oncuechange: null~
+oncut: null~
+ondblclick: null~
+ondrag: null~
+ondragend: null~
+ondragenter: null~
+ondragleave: null~
+ondragover: null~
+ondragstart: null~
+ondrop: null~
+ondurationchange: null~
+onemptied: null~
+onended: null~
+onerror: null~
+onfocus: null~
+oninput: null~
+oninvalid: null~
+onkeydown: null~
+onkeypress: null~
+onkeyup: null~
+onload: null~
+onloadeddata: null~
+onloadedmetadata: null~
+onloadstart: null~
+onmousedown: null~
 onmouseenter: null
 onmouseleave: null
 onmousemove: null
