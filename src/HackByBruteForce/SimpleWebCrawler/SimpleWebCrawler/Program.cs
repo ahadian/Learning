1fe091b0ec14997f50feda742747627c3c1e4a0f
unknown <najim.ahmed@Najim-PC.selise.com.bd>
2015-10-28 13:33:58 +0600

    enam vai thanda

--- src/HackByBruteForce/SimpleWebCrawler/SimpleWebCrawler/Program.cs
+++ src/HackByBruteForce/SimpleWebCrawler/SimpleWebCrawler/Program.cs
@@ -1,35 +1,37 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Mail;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Timers;
+﻿// /*
+//  Copyright (C) Secure Link Services AG. <info@selise.ch>
+//  This file is part of SELISE ECAP (Enterprise Cloud Application Platform)
+//  Any code from SELISE ECAP can not be copied, distributed, reused, published without explicit written and signed permission of management of Secure Link Services AG.
+// */
 
-namespace SimpleWebCrawler
+using System.Diagnostics.CodeAnalysis;
+
+namespace HackByBruteForce
 {
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using System.Net;
+    using System.Net.Mail;
+    using System.Text;
+    using System.Timers;
 
-    class Program
+    internal class Program
     {
-        private static System.Timers.Timer aTimer;
+        private static Timer aTimer;
+
+        private static string Destination, Name, Text1, Text2, Url, TimeInterval;
 
         public static string GetUrl(string url)
         {
-            
             try
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
 
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
-                    Stream receiveStream = response.GetResponseStream();
+                    var receiveStream = response.GetResponseStream();
                     StreamReader readStream = null;
 
                     if (response.CharacterSet == null)
@@ -41,7 +43,7 @@ namespace SimpleWebCrawler
                         readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
                     }
 
-                    string data = readStream.ReadToEnd();
+                    var data = readStream.ReadToEnd();
                     response.Close();
                     readStream.Close();
                     return data;
@@ -49,54 +51,58 @@ namespace SimpleWebCrawler
             }
             catch (Exception e)
             {
-
             }
-            return String.Empty;
+
+            return string.Empty;
         }
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
-            const string chromePath = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
-            var url = Console.ReadLine();
-            string data = GetUrl(url);
-            aTimer = new System.Timers.Timer(10000);
+            Url = ConfigurationManager.AppSettings["Url"];
+            Text1 = ConfigurationManager.AppSettings["KeyWord1"];
+            Text2 = ConfigurationManager.AppSettings["KeyWord2"];
+            Destination = ConfigurationManager.AppSettings["Destination"];
+            Name = ConfigurationManager.AppSettings["Name"];
+            TimeInterval = ConfigurationManager.AppSettings["Interval_SECOND"];
 
-        // Hook up the Elapsed event for the timer.
-        aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
 
-        // Set the Interval to 2 seconds (2000 milliseconds).
-        aTimer.Interval = 5000;
-        aTimer.Enabled = true;
-
-        Console.WriteLine("Press the Enter key to exit the program.");
-        Console.ReadLine();            
+            aTimer = new Timer(10000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.Interval = int.Parse(TimeInterval);
+            aTimer.Enabled = true;
+            Console.WriteLine("Press the Enter key to exit the program.");
+            Console.ReadLine();
         }
 
+        [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:CurlyBracketsMustNotBeOmitted", Justification = "Reviewed. Suppression is OK here."),SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1408:ConditionalExpressionsMustDeclarePrecedence", Justification = "Reviewed. Suppression is OK here.")]
         private static void OnTimedEvent(object sender, ElapsedEventArgs e)
         {
             Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
-            ConfigurationManager.RefreshSection("appSettings");
-            SendMail();
-
+            var data = GetUrl(Url);
+            if (data != null)
+            {
+                int idx1 = data.IndexOf(Text1, StringComparison.InvariantCultureIgnoreCase);
+                int idx2 = data.IndexOf(Text2, StringComparison.InvariantCultureIgnoreCase);
+                bool isActive = (idx1 == -1) || (idx2 == -1);
+                if (isActive) SendMail();
+            }
         }
 
         private static void SendMail()
         {
             var fromAddress = new MailAddress("lesssecure.bd@gmail.com", "Najim");
-            var toAddress = new MailAddress("najim.ju@gmail.com", "Najim");
+            var toAddress = new MailAddress(Destination, Name);
             const string fromPassword = "Abracadabra";
-            const string subject = "Subject";
-            const string body = "Body";
+            const string subject = "Activated!!!";
+            const string body = "The Link is On!!!Hurry Up!";
 
             var smtp = new SmtpClient
             {
                 Host = "smtp.gmail.com",
                 Port = 587,
                 EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
             };
-            smtp.UseDefaultCredentials = false;
 
             using (var message = new MailMessage(fromAddress, toAddress)
             {
