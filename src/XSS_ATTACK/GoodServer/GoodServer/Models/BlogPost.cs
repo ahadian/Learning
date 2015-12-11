11799f785e0be166eb2378ebe947cefd62059e6d
unknown <najim.ju@gmail.com>
2015-12-11 18:29:40 +0600

    XSS

---
+++ src/XSS_ATTACK/GoodServer/GoodServer/Models/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GoodServer.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Post { get; set; }
+    }
+    
+}
