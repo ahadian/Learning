bc33b7bb9bd98cee21f124404cf490d77b069a90
unknown <najim.ju@gmail.com>
2015-12-16 19:36:08 +0600

    ollo

---
+++ src/MongoDbRnd/MongoDbRnd/Controllers/DataManipulationQueryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MongoDbRnd.MakeUpClass;
+
+namespace MongoDbRnd.Controllers
+{
+    public class DataManipulationQueryController : ApiController
+    {
+
+        [HttpPost]
+        public HttpResponseMessage GetFiltered(GetFilteredQuery query)
+        {
+
+            return this.Request.CreateResponse(new DataQueryRepository().GetFiltered(query.EntityName,),HttpStatusCode.Accepted);
+        }
+    }
+
+    public class DataQueryRepository()
+    {
+        
+    }
+}
---
+++ src/MongoDbRnd/MongoDbRnd/MakeUpClass/GetFilteredQuery.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoDbRnd.MakeUpClass
+{
+    public enum FilterType
+    {
+        InnerFilter,Simple
+    }
+    public class DataFilter
+    {
+        public string PropertyName { get; set; }
+        public string Value { get; set; } // value can be deserailized to GetFilteredQuery based on filterType
+
+        public FilterType FilterType { get; set; }
+    }
+    public class GetFilteredQuery 
+    {
+        #region Constructors and Destructors
+
+        public GetFilteredQuery()
+        {
+            this.DataFilters = new DataFilter[] { };
+            //SortOrder = Selise.AppSuite.Common.SortOrder.Ascending;
+            OrderBy = "_id";
+            PageLimit = 20;
+            PageNumber = 0;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public DataFilter[] DataFilters { get; set; }
+
+        public string EntityName { get; set; }
+
+        public string OrderBy { get; set; }
+
+        public int? PageLimit { get; set; }
+
+        public int? PageNumber { get; set; }
+        public string[] Fields { get; set; } 
+
+        //public SortOrder? SortOrder { get; set; }
+
+        #endregion
+
+    }
+}
