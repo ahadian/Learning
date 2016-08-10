5dd3f08a956f8fb94fedf6902ca4f5a42e3c391a
unknown <najim.ahmed@Najim-PC.selise.com.bd>
2016-08-10 16:38:44 +0600

    rabbitwork

---
+++ src/PlayingWitthRabbitMQ/ProducerApp_One/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace ProducerApp_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.HostName = "localhost";
+
+            var conn = factory.CreateConnection();
+
+            var channel = conn.CreateModel();
+        }
+    }
+}
